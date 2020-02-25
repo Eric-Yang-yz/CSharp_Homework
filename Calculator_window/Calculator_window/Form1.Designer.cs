eccc4bbd682b14d939f02fefe42d178e628d9e50
820368256@qq.com <820368256@qq.com>
2020-02-25 15:21:20 +0800

    add calculator_window

---
+++ Calculator_window/Calculator_window/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Calculator_window
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.calculate_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ans_label = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(20, 106);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(160, 25);
+            this.num1.TabIndex = 0;
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(318, 106);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(160, 25);
+            this.num2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBox1.Location = new System.Drawing.Point(212, 106);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(75, 23);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // calculate_button
+            // 
+            this.calculate_button.Font = new System.Drawing.Font("宋体", 18F);
+            this.calculate_button.Location = new System.Drawing.Point(172, 177);
+            this.calculate_button.Name = "calculate_button";
+            this.calculate_button.Size = new System.Drawing.Size(153, 56);
+            this.calculate_button.TabIndex = 3;
+            this.calculate_button.Text = "计算";
+            this.calculate_button.UseVisualStyleBackColor = true;
+            this.calculate_button.Click += new System.EventHandler(this.calculate_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 18F);
+            this.label1.Location = new System.Drawing.Point(12, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "结果：";
+            // 
+            // ans_label
+            // 
+            this.ans_label.AutoSize = true;
+            this.ans_label.Font = new System.Drawing.Font("宋体", 18F);
+            this.ans_label.Location = new System.Drawing.Point(98, 272);
+            this.ans_label.Name = "ans_label";
+            this.ans_label.Size = new System.Drawing.Size(0, 30);
+            this.ans_label.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "数字1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(374, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "数字2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 327);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ans_label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calculate_button);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Name = "Form1";
+            this.Text = "这是一个简单的计算器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button calculate_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ans_label;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Calculator_window/Calculator_window/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_window
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a, b;
+                a = Convert.ToInt32(num1.Text);
+                b = Convert.ToInt32(num2.Text);
+                switch (Convert.ToChar(comboBox1.Text))
+                {
+                    case '+': ans_label.Text = (a + b).ToString(); break;
+                    case '-': ans_label.Text = (a - b).ToString(); break;
+                    case '*': ans_label.Text = (a * b).ToString(); break;
+                    case '/':
+                        if (b == 0) ans_label.Text = "Divider can't be Zero!";
+                        else ans_label.Text = Convert.ToString(Convert.ToDouble(a) / Convert.ToDouble(b));
+                        break;
+                    default:
+                        ans_label.Text = "Operator Error!";
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                ans_label.Text = "Format Error!";
+            }
+            catch (OverflowException)
+            {
+                ans_label.Text = "Overflow Error!";
+            }
+            catch (ArgumentNullException)
+            {
+                ans_label.Text = "NULL Error!";
+            }
+        }
+    }
+}
