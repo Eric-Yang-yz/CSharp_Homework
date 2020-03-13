0dcd3b0369cb045ad3e9fb0a4fcc7495edd79477
820368256@qq.com <820368256@qq.com>
2020-03-13 20:29:12 +0800

    add homework4

---
+++ Homework4/模拟闹钟/模拟闹钟/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4_2
+{
+    public delegate void ClockTick(object sender,TimeArgs args);
+    public delegate void ClockAlarm(object sender,TimeArgs args);//声明两个委托类型
+
+    public class TimeArgs
+    {
+        public int hour { get; set; }
+        public int minute { get; set; }
+        public int second { get; set; }
+    }
+
+    public class AlarmClock//事件发布类
+    {
+        public event ClockTick Tick;
+        public event ClockAlarm Alarm;//定义事件
+        public void Run(TimeArgs AlarmTime)//闹钟启动
+        {
+            TimeArgs CurrentTime = new TimeArgs() { hour = 0, minute = 0, second = 0 };//当前时间
+            while (true)
+            {
+                if (IsTime(AlarmTime,CurrentTime))Alarm(this,AlarmTime);
+                Tick(this,CurrentTime);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+        public bool IsTime(TimeArgs a,TimeArgs b)
+        {
+            return a.hour == b.hour && a.minute == b.minute && a.second == b.second;
+        }
+    }
+
+    public class MyAlarmClock
+    {
+        public AlarmClock Clock1 = new AlarmClock();
+        public MyAlarmClock()//订阅事件
+        {
+            Clock1.Tick += MyTick;
+            Clock1.Alarm += MyAlarm;
+        }
+        void MyTick(object sender,TimeArgs args)
+        {
+            Console.WriteLine("当前时间：[" + args.hour + ":" + args.minute + ":" + args.second + "]");
+            if (args.second < 59) args.second++;
+            else
+            {
+                args.second = 0;
+                args.minute++;
+                if (args.minute == 60)
+                {
+                    args.minute = 0;
+                    args.hour = (args.hour + 1) % 24;
+                }
+            }
+            
+        }
+        void MyAlarm(object sender,TimeArgs args)
+        {
+            for (int i=0;i<10;i++)
+            Console.WriteLine("滴!滴!滴!滴!滴!滴!滴!滴!滴!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyAlarmClock CLK = new MyAlarmClock();
+            TimeArgs AlarmTime = new TimeArgs()
+            {
+                hour = 0,
+                minute = 1,
+                second = 0
+            };
+            CLK.Clock1.Run(AlarmTime);
+        }
+    }
+}
---
+++ Homework4/泛型链表/泛型链表/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4_1
+{
+    public class Node<T>//链表结点
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>//泛型链表类
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void Foreach(Action<T> action)
+        {
+            Node<T> Temp = head;
+            action(Temp.Data);
+            while(Temp != tail)
+            {
+                Temp = Temp.Next;
+                action(Temp.Data);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<double> Glist = new GenericList<double>();
+            Console.WriteLine("请输入5个浮点数！(每行一个)");
+            int i = 0;
+            while(i < 5)
+            {
+                double Data;
+                if (!double.TryParse(Console.ReadLine(), out Data)) Console.WriteLine("非法输入，请重新输入！");
+                else
+                {
+                    i++;
+                    Glist.Add(Data);
+                }
+            }
+            //打印所有元素
+            Console.WriteLine("以下是链表中所有元素：");
+            Glist.Foreach(m => Console.Write(m + " "));
+            Console.WriteLine();
+            //求最大值
+            double max = double.MinValue;
+            Glist.Foreach(m => { if (m > max) max = m; });
+            Console.WriteLine("最大值是：" + max);
+            //求最小值
+            double min = double.MaxValue;
+            Glist.Foreach(m => { if (m < min) min = m; });
+            Console.WriteLine("最小值是：" + min);
+            //求和
+            double sum = 0;
+            Glist.Foreach(m => sum += m);
+            Console.WriteLine("所有数之和是：" + sum);
+        }
+    }
+}
